7f2e7147c206f9ec49fcfa9a3fcb0e247aa57929
Dado-hash <60903402+Dado-hash@users.noreply.github.com>
2025-04-20 12:09:15 +0200

    Starting the tenth module

---
+++ meals/lib/providers/favorites_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]); // i dati all'inizio sono una lista vuota
+
+  bool toggleMealFavoriteStatus(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList(); // remove a meal
+      return false;
+    } else {
+      state = [...state, meal]; // add a meal
+      return true;
+    }
+  }
+}
+
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
